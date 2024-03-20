ad8f758cbe1641ac77f901b62d8e081b09d299d4
Josh Shavers <shaversj@gmail.com>
2024-03-20 08:09:46 -0500

    Initial Commit + Tailwind Setup

---
+++ src/app/cyp/page.js
@@ -0,0 +1,7 @@
+export default function CYPPage() {
+  return (
+    <div>
+      <h1>CYP</h1>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      colors: {
+        "dark-cyan": "#0E8784",
+        "dark-grey-blue": "#333D4B",
+        "pale-orange": "#FDD6BA",
+        "light-cream": "#FEFCF7",
+        grey: "#83888f",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        fraunces: ["var(--font-fraunces)"],
+        barlow: ["var(--font-barlow)"],
       },
     },
   },
