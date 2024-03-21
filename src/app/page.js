e3d3b77973eaecabc3fb330aaabf1a5983e18462
Josh Shavers <shaversj@gmail.com>
2024-03-21 08:31:36 -0500

    Add html for Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,110 @@
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+  return (
+    <>
+      <div>
+        <header>
+          <nav></nav>
+        </header>
+        <main>
+          <div>
+            <h1>Great coffee made simple.</h1>
+            <p>Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.</p>
+            <button>
+              <span>Create your plan</span>
+            </button>
+          </div>
+          <div>
+            <h2>our collection</h2>
+            <div>
+              <div>
+                <img src="" alt="" />
+                <h3>Gran Espresso</h3>
+                <p>Light and flavorful blend with cocoa and black pepper for an intense experience</p>
+              </div>
+              <div>
+                <img src="" alt="" />
+                <h3>Planalto</h3>
+                <p>Brazilian dark roast with rich and velvety body, and hints of fruits and nuts</p>
+              </div>
+              <div>
+                <img src="" alt="" />
+                <h3>Piccollo</h3>
+                <p>Mild and smooth blend featuring notes of toasted almond and dried cherry</p>
+              </div>
+              <div>
+                <img src="" alt="" />
+                <h3>Danche</h3>
+                <p>Ethiopian hand-harvested blend densely packed with vibrant fruit notes</p>
+              </div>
+            </div>
+            <div>
+              <h3>Why choose us?</h3>
+              <p>A large part of our role is choosing which particular coffees will be featured in our range. This means working closely with the best coffee growers to give you a more impactful experience on every level.</p>
+              <div>
+                <div>
+                  <img src="" alt="" />
+                  <h4>Best quality</h4>
+                  <p>Discover an endless variety of the world’s best artisan coffee from each of our roasters.</p>
+                </div>
+                <div>
+                  <img src="" alt="" />
+                  <h4>Exclusive benefits</h4>
+                  <p>Special offers and swag when you subscribe, including 30% off your first shipment.</p>
+                </div>
+                <div>
+                  <img src="" alt="" />
+                  <h4>Free shipping</h4>
+                  <p>We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3>How it works</h3>
+              <div>
+                <div>
+                  <img src="" alt="" />
+                  <h4>Choose your coffee</h4>
+                  <p>Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.</p>
+                </div>
+                <div>
+                  <img src="" alt="" />
+                  <h4>Choose your frequency</h4>
+                  <p>Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.</p>
+                </div>
+                <div>
+                  <img src="" alt="" />
+                  <h4>Receive and enjoy!</h4>
+                  <p>We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.</p>
+                </div>
+              </div>
+            </div>
+            <button>
+              <span>Create your plan</span>
+            </button>
+          </div>
+        </main>
+        <footer>
+          <nav>
+            <div>
+              <img src="" alt="" />
+              <h4>Home</h4>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <h4>About us</h4>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <h4>Create your plan</h4>
+            </div>
+          </nav>
+          <div>
+            <h4>Facebook</h4>
+            <h4>Twitter</h4>
+            <h4>Instagram</h4>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
 }
