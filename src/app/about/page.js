19fc40b051ee19a7868a16d0d747f00f4d425bad
Josh Shavers <shaversj@gmail.com>
2024-04-03 11:03:45 -0500

    Add image

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -20,8 +20,8 @@ export default function About() {
         <picture>
           <source media="(min-width: 1440px)" srcSet={desktop} />
           <source media="(min-width: 768px)" srcSet={tablet} />
-          <source media="(min-width: 375px)" srcSet={mobile} />
-          <img {...rest} className={"h-auto w-full rounded-xl"} alt={"Hero Image"} />
+          {/*<source media="(min-width: 375px)" srcSet={mobile} />*/}
+          <Image src={"/assets/about/mobile/image-hero-whitecup.avif"} {...rest} className={"h-auto w-full rounded-xl"} alt={"Hero Image"} />
         </picture>
       </div>
 
