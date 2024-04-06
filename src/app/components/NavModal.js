153e0af1cb0a680199e8c750c22accd61acf683d
Josh Shavers <shaversj@gmail.com>
2024-04-06 08:59:20 -0500

    Update Link in Nav menu

--- src/app/components/NavModal.js
+++ src/app/components/NavModal.js
@@ -6,7 +6,7 @@ export default function NavModal({ isModalOpenHandler }) {
     <>
       <nav
         className={
-          "bg-gradient-180 absolute left-0 top-24 z-50 flex h-[600px] w-full flex-col items-center space-y-[32px] font-fraunces text-[24px] font-bold text-very-dark-blue "
+          "absolute left-0 top-24 z-50 flex h-[600px] w-full flex-col items-center space-y-[32px] bg-gradient-180 font-fraunces text-[24px] font-bold text-very-dark-blue "
         }
       >
         <Link onClick={isModalOpenHandler} className={"pt-[40px]"} href={"/"}>
@@ -15,7 +15,7 @@ export default function NavModal({ isModalOpenHandler }) {
         <Link onClick={isModalOpenHandler} href={"/about"}>
           <span>About</span>
         </Link>
-        <Link onClick={isModalOpenHandler} href={"/subscribe"}>
+        <Link onClick={isModalOpenHandler} href={"/plan"}>
           <span>Create Your Plan</span>
         </Link>
       </nav>
